01ed55fc4c6d34dc597ea087f3b914aad81a62dc
rhimshahcc <73450321+rhimshahcc@users.noreply.github.com>
2022-03-01 00:56:47 +0000

    Nearest Neighbour Updates
    Updates to Nearest Neighbour.

---
+++ Experimentation/Algorithms/Nearest Neighbour/user_based_sim.m
@@ -0,0 +1,24 @@
+
+function user_based_sim(row_pos,col_pos,D_training)
+
+for n = 1:size(D_training,1) % number of rows
+    
+    if D_training(n,col_pos) ~= 0
+        %compare row row_pos with the current n row
+        % do the ratiings that are used in the sim need to be calculated by
+        % both users?
+        
+        if 
+            
+        end
+        
+        % take a row --> find the other rows which are rated as much
+    end
+    
+end
+
+end
+
+
+
+
