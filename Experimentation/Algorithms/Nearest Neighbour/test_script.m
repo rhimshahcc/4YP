07163f4dc8122af542122060e606b5c5c5148dd2
rhimshahcc <73450321+rhimshahcc@users.noreply.github.com>
2022-03-09 17:39:56 +0000

    Naive Bayes Update
    Naive Bayes update (still contains bugs).

--- Experimentation/Algorithms/Nearest Neighbour/test_script.m
+++ Experimentation/Algorithms/Nearest Neighbour/test_script.m
@@ -9,14 +9,14 @@ addpath('../../Generic Functions')
 % Yahoo_Music_Ratings.txt
 
 % Form the data matrix
-txt_file = '../../Datasets/Movie_Lens_Ratings.txt';
+txt_file = '../../Datasets/Restaurant_Rec_ratings.txt';
 D = readmatrix(txt_file);
 D = D(:,1:3);
 ratings_matrix = make_ratings_matrix(D, txt_file); % Form the ratings matrix
 
 % Form a smaller test matrix
-test_matrix = make_test_matrix(ratings_matrix,1200,1200);
-ratings_matrix = test_matrix;
+%test_matrix = make_test_matrix(ratings_matrix,1200,1200);
+%ratings_matrix = test_matrix;
 
 % Inputs
 split = 4; % number of cross-validation folds
